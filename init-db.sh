350c5b353d65a13a524fcbb11e5b6a470247bbc3
philkry <philkry@users.noreply.github.com>
2024-08-20 09:37:40 +0000

    fixed env names

--- init-db.sh
+++ init-db.sh
@@ -2,20 +2,20 @@
 
 # Variables
 MONGO_HOST=${MONGO_HOST:-"mongodb://mongodb:27017"}
-MONGO_DB_NAME=${DB_NAME:-"mydatabase"}
-MONGO_DB_USER=${DB_USER:-"myuser"}
-MONGO_DB_PASS=${DB_PASS:-"mypassword"}
-MONGO_ROOT_USER=${ROOT_USER:-"root"}
-MONGO_ROOT_PASS=${ROOT_PASS:-"rootpassword"}
+MONGO_DB_NAME=${MONGO_DB_NAME:-"mydatabase"}
+MONGO_DB_USER=${MONGO_DB_USER:-"myuser"}
+MONGO_DB_PASS=${MONGO_DB_PASS:-"mypassword"}
+MONGO_ROOT_USER=${MONGO_ROOT_USER:-"root"}
+MONGO_ROOT_PASS=${MONGO_ROOT_PASS:-"rootpassword"}
 
 # Wait for MongoDB to be ready
-until mongosh $MONGO_HOST/admin -u $ROOT_USER -p $ROOT_PASS --eval "print(\"waited for connection\")"; do
+until mongosh $MONGO_HOST/admin -u $MONGO_ROOT_USER -p $MONGO_ROOT_PASS --eval "print(\"waited for connection\")"; do
   >&2 echo "MongoDB is unavailable - sleeping"
   sleep 2
 done
 
 # Create the database and user
-mongosh $MONGO_HOST/admin -u $ROOT_USER -p $ROOT_PASS --eval "db.getSiblingDB('$DB_NAME').createUser({user: '$DB_USER', pwd: '$DB_PASS', roles: [{role: 'readWrite', db: '$DB_NAME'}]});"
+mongosh $MONGO_HOST/admin -u $MONGO_ROOT_USER -p $MONGO_ROOT_PASS --eval "db.getSiblingDB('$MONGO_DB_NAME').createUser({user: '$MONGO_DB_USER', pwd: '$MONGO_DB_PASS', roles: [{role: 'readWrite', db: '$MONGO_DB_NAME'}]});"
 
 # Exit after the script is executed
 echo "MongoDB user and database created successfully."
